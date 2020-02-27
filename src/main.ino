3f27348dc3e6741974d0cda7d40f29545441f85d
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2020-02-27 02:05:20 +0100

    MQTT communication and basic features completed

--- src/main.ino
+++ src/main.ino
@@ -1,73 +1,110 @@
-/*
- Publishing in the callback
-  - connects to an MQTT server
-  - subscribes to the topic "inTopic"
-  - when a message is received, republishes it to "outTopic"
-  This example shows how to control pins using Ethernet shield via MQTT
-*/
-
-#include <SPI.h>
 #include <UIPEthernet.h>
 #include <PubSubClient.h>
 
+//Compiler instructions
+
+#define RELAY_ON LOW
+#define RELAY_OFF HIGH
+
+#define RELAY_BOARD_SIZE 4
+
+#define IN_TOPIC "/home/garage/g_controller/commands"
+#define OUT_TOPIC "/home/garage/g_controller/confirmations"
+#define ID_CONTROLLER "controller_1"
+#define USERNAME "username"
+#define PASSWORD "password"
+
 // Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED};
 IPAddress server(192, 168, 51, 117);
 
-const int lightPin = 2;
-
-// Callback function header
-void callback(char* topic, byte* payload, unsigned int length);
+int relay[RELAY_BOARD_SIZE] = {2, 3, 4, 5};
 
 EthernetClient ethClient;
-PubSubClient client(server, 1883, callback, ethClient);
+PubSubClient mqttClient(ethClient);
 
-// Callback function
-void callback(char* topic, byte* payload, unsigned int length) {
-    
-  //turn the LED ON if the payload is '1' and publish to the MQTT server a confirmation message
-  if(payload[0] == '2'){
-    pulserelay();
-    client.publish("outTopic", "Light On"); }
-    
-   if(payload[0] == '1'){
-    digitalWrite(lightPin, HIGH);
-    client.publish("outTopic", "Light On"); }
-    
-  //turn the LED OFF if the payload is '0' and publish to the MQTT server a confirmation message
-  if (payload[0] == '0'){
-    digitalWrite(lightPin, LOW);
-    client.publish("outTopic", "Light Off");
+void blinkRelay(int pin)
+{
+  digitalWrite(pin, RELAY_ON);
+  delay(250);
+  digitalWrite(pin, RELAY_OFF);
+  delay(50);
+  Serial.println("OK_" + (char)pin);
+  mqttClient.publish(OUT_TOPIC, "OK_" + (char)pin);
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if ((int)payload[0] - 48 >= 0 && (int)payload[0] - 48 < RELAY_BOARD_SIZE)
+  {
+    blinkRelay(relay[(int)payload[0] - 48]);
+  }
+  else
+  {
+    Serial.println((int)payload[0] - 48);
+    Serial.println("niente da fare bro");
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(ID_CONTROLLER, USERNAME, PASSWORD))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttClient.publish(OUT_TOPIC, "online");
+      // ... and resubscribe
+      mqttClient.subscribe(IN_TOPIC);
     }
-} // void callback
-
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
-  pinMode(lightPin, OUTPUT);
-  digitalWrite(lightPin, HIGH);
   Serial.begin(9600);
-  Serial.println("prova");
 
-  
-  Ethernet.begin(mac);
-
-  Serial.print(Ethernet.localIP());
-  
-  if (client.connect("alessandro")) {
-    Serial.println("connesso");
-    client.publish("outTopic","hello world");
-    client.subscribe("/test/inTopic");
+  for (int i = 0; i < RELAY_BOARD_SIZE; i++)
+  {
+    pinMode(relay[i], OUTPUT);
+    digitalWrite(relay[i], RELAY_OFF);
   }
+
+  mqttClient.setServer(server, 1883);
+  mqttClient.setCallback(callback);
+
+  Ethernet.begin(mac);
+  // Allow the hardware to sort itself out
+  delay(500);
 }
 
 void loop()
 {
-  client.loop();
-}
-
-void pulserelay(){
-    digitalWrite(lightPin, LOW);
-    delay(250);
-    digitalWrite(lightPin, HIGH);
+  if (!mqttClient.connected())
+  {
+    reconnect();
+  }
+  mqttClient.loop();
+  delay(20);
 }
